9fa861bb9c623bda9ce54dc15d9e5f9d4f9db357
Adro Morelos <me@adro.rocks>
2026-02-27 16:17:11 -0600

    feat: add service provider for Laravel auto-discovery

---
+++ src/LaravelModelPresenterServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdroSoftware\LaravelModelPresenter;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelModelPresenterServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
