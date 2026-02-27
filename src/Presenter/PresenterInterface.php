53af820efdcbaacd10e4b28ba48048ec3f22999e
Adro Morelos <me@adro.rocks>
2026-02-27 13:15:12 -0600

    refactor: add generic type annotations to PresenterInterface

--- src/Presenter/PresenterInterface.php
+++ src/Presenter/PresenterInterface.php
@@ -7,4 +7,8 @@ use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Jsonable;
 use JsonSerializable;
 
+/**
+ * @extends ArrayAccess<string, mixed>
+ * @extends Arrayable<string, mixed>
+ */
 interface PresenterInterface extends ArrayAccess, Arrayable, JsonSerializable, Jsonable {}
